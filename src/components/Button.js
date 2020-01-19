2b7ac02c79f0c05516474937633627e0514b14fc
lealinin <lea.linin@gmail.com>
2020-01-18 22:53:42 -0800

    Add buttonClass for danger

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,30 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   let buttonClass = "button";
+
+   if (props.confirm) {
+      buttonClass += " button--confirm";
+   }
+   
+   if (props.danger) {
+      buttonClass += " button--danger";
+   }
+
+   return <button className={buttonClass}>{props.children}</button>;
 }
+
+// export const actionsData = {
+//    onPinTask: action('onPinTask'),
+//    onArchiveTask: action('onArchiveTask'),
+//  };
+ 
+//  export const Default = () => {
+//    return <Task task={{ ...taskData }} {...actionsData} />;
+//  };
+ 
+//  export const Pinned = () => <Task task={{ ...taskData, state: 'TASK_PINNED' }} {...actionsData} />;
+ 
+//  export const Archived = () => (
+//    <Task task={{ ...taskData, state: 'TASK_ARCHIVED' }} {...actionsData} />
+//  );
