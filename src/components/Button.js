6a0c7b3a467824c7a7a238bc34ec10dadf36d38b
lealinin <lea.linin@gmail.com>
2020-01-19 14:46:28 -0800

    Modify props.danger

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,7 +6,7 @@ import "components/Button.scss";
 export default function Button(props) {
    const buttonClass = classnames({
       "button--confirm": props.confirm,
-      "button--danger": props.confirm
+      "button--danger": props.danger
    });
 
    return (
