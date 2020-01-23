6910d99c909df754eaa541efe811fcfa3b2376cd
lealinin <lea.linin@gmail.com>
2020-01-22 17:04:18 -0800

    add get request to fetch days from database

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
 
 import "components/Application.scss";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+// Hard-coded days array
+
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
 const appointments = [
   {
@@ -80,7 +84,16 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
   // console.log(day);
+
+  useEffect(() => {
+    axios.get('/api/days')
+      .then(function (response) {
+        console.log("Inside get request:", response.data);
+        setDays(response.data);
+      })
+  }, []);
 
   return (
     <main className="layout">
