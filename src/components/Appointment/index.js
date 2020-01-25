0fd70fe8347de0645a1f2ae42a7b643327437451
lealinin <lea.linin@gmail.com>
2020-01-25 14:07:58 -0800

    add save function and transition saving and transition show in form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,18 +6,36 @@ import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
 import Form from './Form';
+import Status from './Status';
 
 import useVisualMode from "../../hooks/useVisualMode";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
-const BACK = "BACK";
+const SAVING = "SAVING";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    return interview;
+  }
+
+  // const save = (name, interviewer) => {
+  //   const interview = {
+  //     student: name,
+  //     interviewer
+  //   }
+  //   props.bookInterview(props.id, interview)
+  //   transition(SHOW)    // warning: if bookInterview is async, this must wait until bookInterview is really done
+  // };
 
   // console.log(props.interviewers);
 
@@ -32,12 +50,18 @@ export default function Appointment(props) {
           interviewer={props.interview.interviewer}
         />
       )}
-      {mode === CREATE && (
+      {mode === EMPTY || mode === CREATE && (
         <Form
           interviewers={props.interviewers}
-          onCancel={() => {back()}}
+          onCancel={() => back()}
+          onSave={(name, interviewer) => {
+            transition(SAVING);
+            props.bookInterview(props.id, save(name, interviewer))
+              .then(() => transition(SHOW))
+          }}
         />
       )}
+      {mode === SAVING && <Status message="Saving" />}
     </article>
   );
 }
