23fe697dee175efd7609ae99be0dfde7c2a13231
lealinin <lea.linin@gmail.com>
2020-01-25 14:02:56 -0800

    clean up comments

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,8 +4,6 @@ export default function useVisualMode(initial) {
   const [ mode, setMode ] = useState(initial);
   const [ history, setHistory ] = useState([initial]);
 
-    // When transition is called, we need to add the new mode to our history.
-
   function transition(newMode, replace = false) {
     const newHistory = [...history];
     if (replace) {
@@ -14,19 +12,6 @@ export default function useVisualMode(initial) {
     setHistory([...newHistory, newMode]);
     setMode(newMode);
   }
-
-  // Add a back() function to the Hook that allows us to go back to the previous mode. 
-  // Be sure to add the back property to the object that useVisualMode returns.
-
-  // When back is called, we should set the mode to the previous item in our history array.
-
-  // Every time we add a mode to our history it goes to the top of the stack, this means to 
-  // transition back to the previous mode, all we need to do is remove the last item from the stack, 
-  // and then setMode with the last item in the array.
-
-  // We need to put a constraint on our back function. It should not allow the user to go back past the initial mode. 
-  // This means that our history array will always need to have a length that is great than 1. This test will confirm 
-  // that navigating back from the initial mode will not change the mode value.
 
   function back() {
     if (history.length > 1) {
