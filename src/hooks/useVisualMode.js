ccdb713c86df0ca19bfa8a0882e722413afd9750
lealinin <lea.linin@gmail.com>
2020-01-26 13:59:20 -0800

    add setHistory to prevent stale state

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,6 +4,9 @@ export default function useVisualMode(initial) {
   const [ mode, setMode ] = useState(initial);
   const [ history, setHistory ] = useState([initial]);
 
+  // when transition is called, we need to add the new mode
+  // to our history
+
   function transition(newMode, replace = false) {
     const newHistory = [...history];
     if (replace) {
@@ -12,6 +15,9 @@ export default function useVisualMode(initial) {
     setHistory([...newHistory, newMode]);
     setMode(newMode);
   }
+
+  // when back is called, we should set the mode to the
+  // previous item in our history array
 
   function back() {
     if (history.length > 1) {
