c37d0b563112db456ce076176d6395e62cc2b87b
lealinin <lea.linin@gmail.com>
2020-01-26 14:16:59 -0800

    delete comments

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,8 +4,6 @@ export default function useVisualMode(initial) {
   const [ mode, setMode ] = useState(initial);
   const [ history, setHistory ] = useState([initial]);
 
-  // when transition is called, we need to add the new mode
-  // to our history
 
   function transition(newMode, replace = false) {
     const newHistory = [...history];
@@ -15,9 +13,6 @@ export default function useVisualMode(initial) {
     setHistory([...newHistory, newMode]);
     setMode(newMode);
   }
-
-  // when back is called, we should set the mode to the
-  // previous item in our history array
 
   function back() {
     if (history.length > 1) {
