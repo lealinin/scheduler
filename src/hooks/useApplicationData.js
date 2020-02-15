c972bb94c0b65e8b3dc2a2e3d10f9b964d93483a
lealinin <lea.linin@gmail.com>
2020-02-15 01:06:32 -0800

    move reducer function to application.js and import constants and reducer from src/reducers/application.js

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,52 +1,12 @@
 import { useReducer, useEffect } from "react";
 import axios from "axios";
+import reducer, {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW
+} from "../reducers/application";
 
-
-const SET_DAY = "SET_DAY";
-const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-const SET_INTERVIEW = "SET_INTERVIEW";
-
-function reducer(state, action) {
-  switch (action.type) {
-    case SET_DAY:
-      return { ...state, day: action.day }
-    case SET_APPLICATION_DATA:
-      return { ...state, days: action.days, appointments: action.appointments, interviewers: action.interviewers }
-    case SET_INTERVIEW: {
-
-      let appointment = {}
-
-      if (action.interview) {
-        appointment = { ...state.appointments[action.id], interview: { ...action.interview } }
-      } else {
-        appointment = { ...state.appointments[action.id], interview: action.interview }
-      }
-
-      const appointments = { ...state.appointments, [action.id]: appointment }
-
-      const updateSpots = state.days.map((day) => {
-        for (let appointment of day.appointments) {
-          if (action.id === appointment) {
-            if (action.interview && !state.appointments[action.id].interview) {
-              return { ...day, spots: day.spots - 1 };
-            } else if (!action.interview && state.appointments[action.id].interview) {
-              return { ...day, spots: day.spots + 1 };
-            }
-          }
-        }
-        return day
-      })
-      return { ...state, appointments, days: updateSpots }
-
-    }
-    default:
-      throw new Error(
-        `Tried to reduce with unsupported action type: ${action.type}`
-      );
-  }
-}
-
-export default function useApplicationData(initial) {
+export default function useApplicationData() {
 
   const [state, dispatch] = useReducer(reducer, {
     day: "Monday",
